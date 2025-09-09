5755bc4cef2bc9f95dc7a1b9e130c918d1a07d45
smokiinape <amanblvck@gmail.com>
2025-09-10 00:33:18 +0200

    Update project file paths to reflect new directory structure and regenerate assembly info

--- backend/backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a574f3dc48da2405752c94f38c16bb6eecb7d9ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff764b961afb32270b5fa7cf1b9dba97331e07c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
